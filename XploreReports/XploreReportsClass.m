0b4ebe205ebe6307f10b7be70511894b7c6914c8
Antoine Rosset <rossetantoine@bluewin.ch>
2010-04-15 11:03:08 +0000

    support for web url XPlore Reports in reportURL field
    git-svn-id: https://svn.code.sf.net/p/osirixplugins/code@59 bdd3870d-6ba2-4b59-8605-1de7af000970

--- XploreReports/XploreReportsClass.m
+++ XploreReports/XploreReportsClass.m
@@ -3,20 +3,52 @@
 //
 
 #import "XploreReportsClass.h"
+#import <OsiriX Headers/Notifications.h>
+#import <OsiriX Headers/DicomImage.h>
+#import <OsiriX Headers/DicomStudy.h>
+
+#define ADDRESS @"172.16.1.13"
 
 @implementation XploreReportsClass
 
 - (void) initPlugin
 {
+	NSLog( @"------ XploreReportsClass initPlugin");
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(databaseAddition:) name: OsirixAddToDBCompleteNotification object:NULL];
+}
+
+-(void)databaseAddition:(NSNotification*)notification
+{
+	NSArray* addedImages = [[notification userInfo] objectForKey: OsirixAddToDBCompleteNotificationImagesArray];
+	
+	NSMutableArray *studies = [NSMutableArray array];
+	
+	@try
+	{
+		for( DicomImage* image in addedImages)
+			if( [studies containsObject: [image valueForKeyPath: @"series.study"]] == NO)
+				[studies addObject: [image valueForKeyPath: @"series.study"]];
+		
+		for( DicomStudy* study in studies)
+			if( [study valueForKey: @"reportURL"] == nil || [[study valueForKey: @"reportURL"] length] == 0)
+			{
+				NSString *patientID = [study valueForKey: @"patientID"];
+				
+				[study setValue: [NSString stringWithFormat: @"http://%@/xploreintranet/Patients/PatientExamens.asp?PatientID=%@", ADDRESS, patientID] forKey: @"reportURL"];
+			}
+	}
+	
+	@catch (NSException* e)
+	{
+		NSLog( @"***** exception in %s: %@", __PRETTY_FUNCTION__, e);
+	}
 }
 
 - (long) filterImage:(NSString*) menuName
 {
 	NSString *patientID = [[[viewerController fileList] lastObject] valueForKeyPath: @"series.study.patientID"];
 	
-	NSString *address = @"172.16.1.13";
-	
-	NSURL *url = [NSURL URLWithString: [NSString stringWithFormat: @"http://%@/xploreintranet/Patients/PatientExamens.asp?PatientID=%@", address, patientID]];
+	NSURL *url = [NSURL URLWithString: [NSString stringWithFormat: @"http://%@/xploreintranet/Patients/PatientExamens.asp?PatientID=%@", ADDRESS, patientID]];
 	
 	[[NSWorkspace sharedWorkspace] openURL: url];
 	
